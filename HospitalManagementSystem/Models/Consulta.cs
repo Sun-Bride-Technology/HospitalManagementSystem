db91c17437ba340fdf0c2cde91757230552b4395
Raul Arturo Salomon Iglesias <iglesiasraularturo2001@outlook.com>
2022-02-23 21:24:09 -0600

    Add: Medical Consultation Class and Property definition

---
+++ HospitalManagementSystem/Models/Consulta.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalManagementSystem.Models
+{
+    class Consulta
+    {
+        public int Id { get; set; }
+        public int IdAsignacion { get; set; }
+        public DateTime Fecha { get; set; }
+        public string Sintomas { get; set; }
+        public string Diagnostico { get; set; }
+        public string Tratamiento { get; set; }
+
+    }
+}
--- HospitalManagementSystem/Models/Medico.cs
+++ HospitalManagementSystem/Models/Medico.cs
@@ -8,5 +8,13 @@ namespace HospitalManagementSystem.Models
 {
     class Medico
     {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string ApPaterno { get; set; }
+        public string ApMaterno { get; set; }
+        public int Edad { get; set; }
+        public string Especialidad { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
     }
 }
