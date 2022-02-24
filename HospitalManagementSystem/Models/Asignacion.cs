9098abaec21672822b9d147bb9fc4686e2e1d2f1
pedro-aguilarha <pedro.aguilarha@uanl.edu.mx>
2022-02-23 21:50:10 -0600

    Definicion de clases de pacientes y asignaciones

---
+++ HospitalManagementSystem/Models/Asignacion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalManagementSystem.Models
+{
+    class Asignacion
+    {
+        public int IdAsignacion { get; set; }
+        public int IdPaciente { get; set; }
+        public int IdMedico { get; set; }
+        public int Cama { get; set; }
+        public DateTime FechaAlta { get; set; }
+        public Nullable<DateTime> FechaBaja { get; set; }
+    }
+}
---
+++ HospitalManagementSystem/Models/Paciente.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalManagementSystem.Models
+{
+    class Paciente
+    {
+        public int IdPaciente { get; set; }
+        public string Nombre { get; set; }
+        public string ApPaterno { get; set; }
+        public string ApMaterno { get; set; }
+        public int Edad { get; set; }
+        public char Sexo { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+    }
+}
