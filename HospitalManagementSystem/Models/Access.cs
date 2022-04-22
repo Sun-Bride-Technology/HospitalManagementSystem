406296956a3aa1d1762412547016ef8bf8e1d1d1
RaulSalomonIgl <iglesiasraularturo2001@outlook.com>
2022-04-22 02:04:06 -0500

    Add: 'Login' class

--- HospitalManagementSystem/Models/Access.cs
+++ HospitalManagementSystem/Models/Access.cs
@@ -7,6 +7,19 @@ using System.ComponentModel.DataAnnotations;
 
 namespace HospitalManagementSystem.Models
 {
+    class Login
+    {
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [EmailAddress]
+        [StringLength(50, ErrorMessage = "El {0} debe contener al menos {2} caracteres y maximo {1} caracteres", MinimumLength = 5)]
+        [Display(Name = "Correo Electrónico")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [StringLength(1000, ErrorMessage = "La {0} debe contener al menos {2} caracteres y maximo {1} caracteres", MinimumLength = 5)]
+        [Display(Name = "Contraseña")]
+        public string Password { get; set; }
+    }
     class User
     {
         public int Id { get; set; }
